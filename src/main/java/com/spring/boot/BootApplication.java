627de69b064b4fd708c454c055264cd725965b2f
lyj <leeyujin95y@naver.com>
2020-10-22 12:20:46 +0900

    인텔리제이 깃 테스트

--- src/main/java/com/spring/boot/BootApplication.java
+++ src/main/java/com/spring/boot/BootApplication.java
@@ -8,7 +8,7 @@ public class BootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
-        System.out.println("나는 할 수 있따.");
+        System.out.println("나는 할 수 있따.2");
     }
 
 }
